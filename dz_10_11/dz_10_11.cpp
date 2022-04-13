412ef90b0377de243a44d3caa43e444606a65f3d
zonky <Juphy@yandex.ru>
2022-04-13 10:36:41 +0300

    Добавьте файлы проекта.

---
+++ dz_10_11/dz_10_11.cpp
@@ -0,0 +1,17 @@
+﻿#include "human.h"
+
+
+int main() {
+
+	Student a("Bob", "secondBob", "midBob", 123, 1);
+	Boss Bob("Bob", "secondBob", "midBob", 123, 12345);
+	a.print();
+	Bob.print();
+
+
+	Parrot dd(1, "red");
+	Cat Bersik(20, "yellow");
+	dd.voice();
+	Bersik.voice();
+	return 0;
+}
---
+++ dz_10_11/human.h
@@ -0,0 +1,126 @@
+#ifndef human_H
+#define human_H
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+//dz 10 
+class Human {
+protected:
+	string surname, name, midname;
+	int age;
+public:
+	Human() {
+		name = "";
+		surname = "";
+		midname = "";
+		age = 0;
+
+	}
+	Human(string name, string surname, string midname, int age) {
+		this->name = name;
+		this->midname = midname;
+		this->surname = surname;
+		this->age = age;
+
+	}
+	virtual void print() = 0;
+	~Human() {}
+};
+
+
+
+
+class Student :public Human {
+private:
+	bool on_lesson;
+public:
+	Student() :Human() {
+		on_lesson = 0;
+	}
+	Student(string name, string surname, string midname, int age, bool on_lesson) :Human(name, surname, midname, age) {
+		this->on_lesson = on_lesson;
+	}
+	void print() override {
+		cout << name << "\t" << surname << "\t" << midname << "\t" << age << "\t" << on_lesson << endl;
+	}
+
+};
+
+class Boss :public Human {
+private:
+	int  number_of_workers;
+public:
+	Boss() :Human() {
+		number_of_workers = 0;
+	}
+	Boss(string name, string surname, string midname, int age, int number_of_workers) :Human(name, surname, midname, age) {
+		this->number_of_workers = number_of_workers;
+	}
+	void print() override {
+		cout << name << "\t" << surname << "\t" << midname << "\t" << age << "\t" << number_of_workers << endl;
+	}
+
+};
+
+//11
+
+class Animal {
+protected:
+	int age;
+	string color;
+public:
+	Animal(int age, string color) {
+		this->age = age;
+		this->color = color;
+
+	}
+	virtual void voice() = 0;
+
+
+};
+
+
+
+class Parrot :Animal {
+public:
+	Parrot(int age, string color) :Animal(age, color) {
+		this->age = age;
+		this->color = color;
+	}
+	void voice() override {
+		cout << "sing\n";
+	}
+};
+
+
+class Dog :Animal {
+public:
+	Dog(int age, string color) :Animal(age, color) {
+		this->age = age;
+		this->color = color;
+	}
+	void voice() override {
+		cout << "Gawg\n";
+	}
+};
+
+
+class Cat :Animal {
+public:
+	Cat(int age, string color) :Animal(age, color) {
+		this->age = age;
+		this->color = color;
+	}
+	void voice() override {
+		cout << "Meow\n";
+	}
+};
+
+
+#endif // !HEADER_H
